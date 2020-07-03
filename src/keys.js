fb7072c5c36f262411a741b1caca570617e89546
towonk <arturo@towonk.com>
2020-07-03 03:54:09 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -2,9 +2,9 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: 'basetewys.cyb4em7qrkmo.us-east-2.rds.amazonaws.com',
-        user: 'admin',
-        password: 'mT000229',
+        host: 'localhost',
+        user: 'pece',
+        password: '12345678',
         database: 'database_links'
     }
 
